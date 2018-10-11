1021c096b262fab8a1a189f0dcf78d9cfa4c52d0
Elias Christen <elias_christen@gmx.ch>
2018-10-11 22:55:20 +0800

    Added new API for Viewer Statistic

--- app/src/main/java/com/coderbunker/kioskapp/StatisticsActivity.java
+++ app/src/main/java/com/coderbunker/kioskapp/StatisticsActivity.java
@@ -2,13 +2,11 @@ package com.coderbunker.kioskapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.MenuItem;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -18,6 +16,14 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class StatisticsActivity extends Activity {
     private int countViewers = 0;
@@ -36,24 +42,52 @@ public class StatisticsActivity extends Activity {
 
             private void createViewerPerDayStatistic(List<Viewer> viewers) {
                 int numberOfDaysFromCurrentMonth = new GregorianCalendar().getActualMaximum(Calendar.DATE);
-                GraphView graphView = configurateGraphView(numberOfDaysFromCurrentMonth);
-                List<DataPoint> dataPoints = addDataToGraph(viewers, numberOfDaysFromCurrentMonth);
-                LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints.toArray(new
-                        DataPoint[dataPoints.size()]));
-                graphView.addSeries(series);
-            }
-
-            private List<DataPoint> addDataToGraph(List<Viewer> viewers, int numberOfDaysFromCurrentMonth) {
-                List<DataPoint> dataPoints = new ArrayList<>();
+                LineChartView lineChartView = findViewById(R.id.chart);
+                List yAxisValues = new ArrayList();
+                List axisValues = new ArrayList();
+                Line line = new Line(yAxisValues);
                 for (int dayOfMonthCounter = 1; dayOfMonthCounter <= numberOfDaysFromCurrentMonth; dayOfMonthCounter++) {
                     for (Viewer viewer : viewers) {
                         compareDayOfMonthCounterAndViewerDate(viewer, dayOfMonthCounter);
                     }
-                    dataPoints.add(new DataPoint(dayOfMonthCounter, countViewers));
+                    yAxisValues.add(new PointValue(dayOfMonthCounter - 1, countViewers));
+                    axisValues.add(dayOfMonthCounter - 1, new AxisValue(dayOfMonthCounter - 1).setLabel(dayOfMonthCounter + ""));
                     countViewers = 0;
                 }
-                return dataPoints;
+                List lines = new ArrayList();
+                lines.add(line);
+                LineChartData data = new LineChartData();
+                data.setLines(lines);
+
+                configuratorXAxis(axisValues, data);
+                configuratorYAxis(data);
+                configuratorLineChartView(lineChartView, data);
             }
+
+            private void configuratorLineChartView(LineChartView lineChartView, LineChartData data) {
+                lineChartView.setLineChartData(data);
+                Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+                lineChartView.setMaximumViewport(viewport);
+                lineChartView.setCurrentViewport(viewport);
+            }
+
+            private void configuratorYAxis(LineChartData data) {
+                Axis yAxis = new Axis();
+                yAxis.setName("Viewers");
+                yAxis.setTextColor(Color.parseColor("#03A9F4"));
+                yAxis.setTextSize(20);
+                data.setAxisYLeft(yAxis);
+            }
+
+            private void configuratorXAxis(List axisValues, LineChartData data) {
+                Axis axis = new Axis();
+                axis.setName("Day of Month");
+                axis.setValues(axisValues);
+                axis.setTextSize(20);
+                axis.setTextColor(Color.parseColor("#03A9F4"));
+                data.setAxisXBottom(axis);
+            }
+
 
             private void compareDayOfMonthCounterAndViewerDate(Viewer viewer, int dayOfMonthCounter) {
                 try {
@@ -71,20 +105,6 @@ public class StatisticsActivity extends Activity {
                 } catch (ParseException e) {
                     System.out.println(e.getStackTrace());
                 }
-            }
-
-            private GraphView configurateGraphView(int numOfDaysOfCurrentMonth) {
-                GraphView graph = findViewById(R.id.graph);
-                graph.getViewport().setXAxisBoundsManual(true);
-                graph.getViewport().setMinX(1);
-                graph.getViewport().setMaxX(numOfDaysOfCurrentMonth);
-                graph.setTitle("Viewers per Day");
-                graph.getGridLabelRenderer().setHorizontalAxisTitle("Days of Month");
-                graph.getGridLabelRenderer().setVerticalAxisTitle("Viewers");
-                graph.getGridLabelRenderer().setHorizontalAxisTitleTextSize(30);
-                graph.getGridLabelRenderer().setVerticalAxisTitleTextSize(30);
-                graph.setTitleTextSize(50);
-                return graph;
             }
         });
 
