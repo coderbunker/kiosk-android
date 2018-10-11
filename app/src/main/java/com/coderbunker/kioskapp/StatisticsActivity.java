33f157c5df94f9973c52c0331c65891b5a8127e4
Elias Christen <elias_christen@gmx.ch>
2018-10-11 14:11:07 +0800

    Added first statistic to Statistics Activity (Viewers per Day)

--- app/src/main/java/com/coderbunker/kioskapp/StatisticsActivity.java
+++ app/src/main/java/com/coderbunker/kioskapp/StatisticsActivity.java
@@ -6,25 +6,91 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.MenuItem;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class StatisticsActivity extends Activity {
+    private DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private int countViewers = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         DatabaseConnection db = new DatabaseConnection();
         db.readViewer(new DatabaseConnection.OnViewersReceived() {
             @Override
             public void onViewersReceived(List<Viewer> viewers) {
-                for (Viewer v : viewers) {
-                    System.out.println(v.getStartTime() + " " + v.getEndTime());
+                createViewerPerDayStatistic(viewers);
+            }
+
+            private void createViewerPerDayStatistic(List<Viewer> viewers) {
+                int numberOfDaysFromCurrentMonth = new GregorianCalendar().getActualMaximum(Calendar.DATE);
+                GraphView graphView = configurateGraphView(numberOfDaysFromCurrentMonth);
+                List<DataPoint> dataPoints = addDataToGraph(viewers, numberOfDaysFromCurrentMonth);
+                LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints.toArray(new
+                        DataPoint[dataPoints.size()]));
+                graphView.addSeries(series);
+            }
+
+            private List<DataPoint> addDataToGraph(List<Viewer> viewers, int numberOfDaysFromCurrentMonth) {
+                List<DataPoint> dataPoints = new ArrayList<>();
+                for (int dayOfMonthCounter = 1; dayOfMonthCounter <= numberOfDaysFromCurrentMonth; dayOfMonthCounter++) {
+                    for (Viewer viewer : viewers) {
+                        compareDayOfMonthCounterAndViewerDate(viewer, dayOfMonthCounter);
+                    }
+                    dataPoints.add(new DataPoint(dayOfMonthCounter, countViewers));
+                    countViewers = 0;
                 }
+                return dataPoints;
+            }
+
+            private void compareDayOfMonthCounterAndViewerDate(Viewer viewer, int dayOfMonthCounter) {
+                try {
+                    Date dateOfViewer = format.parse(viewer.getDateTime());
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(dateOfViewer);
+                    int monthOfViewer = cal.get(Calendar.MONTH) + 1;
+                    int dayOfViewer = cal.get(Calendar.DAY_OF_MONTH);
+                    int monthToday = new Date().getMonth() + 1;
+
+                    if (monthOfViewer == monthToday && dayOfViewer == dayOfMonthCounter) {
+                        countViewers++;
+                    }
+                } catch (ParseException e) {
+                    System.out.println(e.getStackTrace());
+                }
+            }
+
+            private GraphView configurateGraphView(int numOfDaysOfCurrentMonth) {
+                GraphView graph = findViewById(R.id.graph);
+                graph.getViewport().setXAxisBoundsManual(true);
+                graph.getViewport().setMinX(1);
+                graph.getViewport().setMaxX(numOfDaysOfCurrentMonth);
+                graph.setTitle("Viewers per Day");
+                graph.getGridLabelRenderer().setHorizontalAxisTitle("Days of Month");
+                graph.getGridLabelRenderer().setVerticalAxisTitle("Viewers");
+                graph.getGridLabelRenderer().setHorizontalAxisTitleTextSize(30);
+                graph.getGridLabelRenderer().setVerticalAxisTitleTextSize(30);
+                graph.setTitleTextSize(50);
+                return graph;
             }
         });
 
 
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/com/coderbunker/kioskapp/ViewerObserver.java
+++ app/src/main/java/com/coderbunker/kioskapp/ViewerObserver.java
@@ -6,6 +6,8 @@ import android.widget.TextView;
 
 import com.coderbunker.kioskapp.facerecognition.FaceDetectionListener;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.Observable;
 import java.util.Observer;
@@ -18,6 +20,8 @@ public class ViewerObserver implements Observer {
     private WebViewVideoReader webViewVideoReader;
     private Queue<Viewer> viewers = new LinkedList<>();
     private int currentFaceCounter;
+    private static SimpleDateFormat formatter = new SimpleDateFormat(
+            "yyyy-MM-dd HH:mm:ss");
 
     public ViewerObserver(TextView faceCounterView, WebView webView) {
         this.faceCounterView = faceCounterView;
@@ -26,37 +30,40 @@ public class ViewerObserver implements Observer {
 
     @Override
     public void update(Observable observable, Object args) {
+
         if (observable instanceof FaceDetectionListener) {
             Camera.Face[] faces = ((Camera.Face[]) args);
             final int faceCounter = faces.length;
             faceCounterView.setText("Current faces: " + faceCounter);
 
-                if (faceCounter > currentFaceCounter) {
-                    webViewVideoReader.runWithCurrentTime(new WebViewVideoReader.VideoProgressObserver() {
-                        @Override
-                        public void onProgressRead(WebViewVideoReader.VideoInfo video) {
-                            Viewer viewer = new Viewer();
-                            viewer.setStartTime(video.getCurrentTime());
-                            viewer.setVideoUrl(video.getVideoSrc());
-                            viewers.add(viewer);
+            if (faceCounter > currentFaceCounter) {
+                webViewVideoReader.runWithCurrentTime(new WebViewVideoReader.VideoProgressObserver() {
+                    @Override
+                    public void onProgressRead(WebViewVideoReader.VideoInfo video) {
+                        Viewer viewer = new Viewer();
+                        viewer.setStartTime(video.getCurrentTime());
+                        viewer.setVideoUrl(video.getVideoSrc());
+                        String currentDateTime = formatter.format( new Date());
+                        viewer.setDateTime(currentDateTime);
+                        viewers.add(viewer);
 
+                    }
+                });
+                currentFaceCounter = faceCounter;
+            } else if (faceCounter < currentFaceCounter) {
+                webViewVideoReader.runWithCurrentTime(new WebViewVideoReader.VideoProgressObserver() {
+                    @Override
+                    public void onProgressRead(WebViewVideoReader.VideoInfo video) {
+                        Viewer viewer = viewers.element();
+                        viewer.setEndTime(video.getCurrentTime());
+                        if (viewer.getStartTime() + 5 <= viewer.getEndTime()) {
+                            dbc.addViewer(viewer);
                         }
-                    });
-                    currentFaceCounter = faceCounter;
-                } else if (faceCounter < currentFaceCounter) {
-                    webViewVideoReader.runWithCurrentTime(new WebViewVideoReader.VideoProgressObserver() {
-                        @Override
-                        public void onProgressRead(WebViewVideoReader.VideoInfo video) {
-                            Viewer viewer = viewers.element();
-                            viewer.setEndTime(video.getCurrentTime());
-                            if (viewer.getStartTime() + 5 <= viewer.getEndTime()) {
-                                dbc.addViewer(viewer);
-                            }
-                            viewers.remove(viewer);
-                            currentFaceCounter =faceCounter;
-                        }
-                    });
-                }
+                        viewers.remove(viewer);
+                        currentFaceCounter = faceCounter;
+                    }
+                });
+            }
 
 
         }
