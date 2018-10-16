28ba7825d196de03f4ea517716600d218de69a53
Alexander Wyss <alexsilvan.wyss@gmail.com>
2018-10-16 10:31:56 +0800

    AES encryption class, with example usage

--- app/src/androidTest/java/com/coderbunker/kioskapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/coderbunker/kioskapp/ExampleInstrumentedTest.java
@@ -4,8 +4,16 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -17,10 +25,20 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    public void useAppContext() throws Exception {
+        String passphrase = "123456";
+        int iterationCount = 1000;
+        int keySize = 128;
+        String salt = "9876";
+        String ciphertext = "uj9QkQg7gdhZxRjUcqRt3Q==";
+        String iv = "12345678901234561234567890123456";
 
-        assertEquals("com.coderbunker.kioskapp", appContext.getPackageName());
+        AesUtil aesUtil = new AesUtil(keySize, iterationCount);
+        String encrypt = aesUtil.encrypt(salt, iv, passphrase, "Hello World, i hope this works.");
+
+        System.out.println(encrypt);
+
+        String plaintext = aesUtil.decrypt(salt, iv, passphrase, encrypt);
+        System.out.println(plaintext);
     }
 }
